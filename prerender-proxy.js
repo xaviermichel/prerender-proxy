06f79d9b23262f3b4fc96161f6e7942e8fa51db7
Xavier MICHEL <xavier.michel.mx440@gmail.com>
2016-10-05 21:32:18 +0200

    fix conf

--- prerender-proxy.js
+++ prerender-proxy.js
@@ -1,15 +1,11 @@
 var prerender = require('prerender/lib/index');
 
-// override port for prerender
-process.env.PORT = process.env.LISTENPORT || 3000;
-process.env.HOST = process.env.LISTENHOST;
-
+// override default values
+process.env.RESOURCE_DOWNLOAD_TIMEOUT = 60 * 1000;
+process.env.WAIT_AFTER_LAST_REQUEST = 1000;
+process.env.JS_TIMEOUT = 60 * 1000;
 
 var server = prerender({
-    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS || 2,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 100,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
 
 server.use(prerender.removeScriptTags());
